d2a1125b00c2ab8d5c4de557547f184a9675589e
Guido A.J. Stevens <guido.stevens@cosent.nl>
2014-04-22 20:45:33 +0000

    pep8 + cleanup

--- setup.py
+++ setup.py
@@ -3,36 +3,35 @@ import os
 
 version = '0.1dev'
 
-setup(name='ploneintranet.portlets',
-      version=version,
-      description="Experimental layout system based on portlets",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='ploneintranet.portlets',
+    version=version,
+    description="Experimental layout system based on portlets",
+    long_description=open("README.txt").read() + "\n" +
+    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['ploneintranet'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
+    ],
+    keywords='',
+    author='Guido Stevens',
+    author_email='guido.stevens@cosent.net',
+    url='https://github.com/ploneintranet/ploneintranet.portlets/',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['ploneintranet'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
       # -*- Entry points: -*-
 
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
-      )
+)
